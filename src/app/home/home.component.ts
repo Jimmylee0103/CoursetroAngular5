1f0de36cac9c5657757b5932e1353fdc3c2ef029
Jimmy Lee <jimmylee0103@gmail.com>
2018-11-08 21:55:19 -0600

    checking in

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { trigger, style, transition, animate, keyframes, query, stagger } from '@angular/animations';
+import { DataService } from '../data.service';
 
 @Component({
   selector: 'app-home',
@@ -10,12 +11,18 @@ import { trigger, style, transition, animate, keyframes, query, stagger } from '
       transition('* => *', [
 
         query(':enter', style({ opacity: 0 }), { optional: true }),
-
         query(':enter', stagger('300ms', [
           animate('.6s ease-in', keyframes([
             style({ opacity: 0, transform: 'translateY(-75%)', offset: 0 }),
             style({ opacity: .5, transform: 'translateY(35px)', offset: 0.3 }),
             style({ opacity: 1, transform: 'translateY(0)', offset: 1.0 }),
+          ]))]), { optional: true }),
+
+        query(':leave', stagger('300ms', [
+          animate('.6s ease-out', keyframes([
+            style({ opacity: 1, transform: 'translateY(0)', offset: 0 }),
+            style({ opacity: .5, transform: 'translateY(35px)', offset: 0.3 }),
+            style({ opacity: 0, transform: 'translateY(-75%)', offset: 1.0 }),
           ]))]), { optional: true })
       ])
     ])
@@ -24,14 +31,16 @@ import { trigger, style, transition, animate, keyframes, query, stagger } from '
 
 export class HomeComponent implements OnInit {
 
-  itemCount: number = 4;
+  itemCount: number;
   btnText: string = 'Add an Item';
   goalText: string = 'My first life goal';
-  goals = ['Jimmys first life goal', 'Jimmys second life goal','Jimmys thrid life goal'];
+  goals = [];
 
-  constructor() { }
+  constructor(private _data: DataService) { }
 
   ngOnInit() {
+    this._data.goal.subscribe(res => this.goals = res);
+    this._data.changeGoal(this.goals);
     this.itemCount = this.goals.length;
   }
 
@@ -39,9 +48,13 @@ export class HomeComponent implements OnInit {
     this.goals.push(this.goalText);
     this.goalText = "";
     this.itemCount = this.goals.length;
+    this._data.changeGoal(this.goals);
   }
 
-  removeItem() {
+  removeItem(i) {
+    this.goals.splice(i, 1);
+    this._data.changeGoal(this.goals);
+    this.itemCount = this.goals.length;
   }
 
 }
